862813f872cc332b8d513e913b88ae38ae291b8d
Joseph Chalabi <chalabi@Macintosh.local>
2022-01-11 02:18:54 -0700

    Added Snapshot automation & upgrade options

--- upgrades.sh
+++ upgrades.sh
@@ -1,8 +1,20 @@
 while true; do
+    read -p "Please select the network you would like to upgrade" yn
+    case $yn in
+        [Aa]* ) export COSMOS_NETWORK=.akash; break;;
+        [Ss]* ) export COSMOS_NETWORK=.sentinel break;;
+        [Oo]* ) export COSMOS_NETWORK=.osmosis break;;
+        * ) echo "Please select your network.";;
+    esac
+done
+
+while true; do
     read -p "Do you wish to install the updated genesis?" yn
     case $yn in
-        [Yy]* ) wget $GENESIS; break;;
+        [Yy]* ) rm ~/$COSMOS_NETWORK/config/genesis.json
+		wget $GENESIS -P ~/$COSMOS_NETWORK/config/; break;;
         [Nn]* ) exit;;
+	[Ss]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
@@ -13,6 +25,7 @@ while true; do
     case $yn in
         [Yy]* ) akash unsafe-reset-all; break;;
         [Nn]* ) exit;;
+	[Ss]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
