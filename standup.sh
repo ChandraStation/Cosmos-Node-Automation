26e3dbd3744f20764a9bc85cdc6ddf55153c9101
Bailey Bellows <parsec@macbook.lan>
2022-01-13 11:54:23 -0700

    Added Chihuahua

--- standup.sh
+++ standup.sh
@@ -20,7 +20,7 @@ cat << "EOF"
 
 EOF
 
-# OS Select 
+# OS Select
 
 PS3='Select your OS: '
 options=("Arch Linux" "Ubuntu" "My OS is set up" "Quit")
@@ -34,8 +34,8 @@ do
         "Ubuntu")
             apt-get update -y && apt install -y tar unzip wget curl gcc make snap && snapd install go --classic; break;;
 
-	"My OS is set up")
-		break;;
+        "My OS is set up")
+                break;;
         "Quit")
             exit;;
 
@@ -45,7 +45,7 @@ done
 # Network Select
 
 PS3='Select a network: '
-options=("Akash" "Sentinel" "Chihuahua" "Comdex" "Dig" "e-Money" "G-Bridge" "Omniflix" "Osmosis" "Sentinel" "Quit")
+options=("Akash" "Chihuahua" "Comdex" "Dig" "e-Money" "G-Bridge" "Omniflix" "Osmosis" "Sentinel" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -94,19 +94,64 @@ do
                                         sudo systemctl start akash && journalctl -u akash -f; break;;
 
                                 "Testnet")
-                                        echo "Sorry this is still a work in progress"
-                                        ;;
-
+                                        break;;
                                 "Go Back")
-                                        exit;;
+                                        break;;
                         esac
                 done
 
             ;;
         "Chihuahua")
-            echo "you chose choice 2"
+                PS3='Would you like to run a Mainnet or Testnet Node?'
+                options=("Mainnet" "Testnet" "Go Back")
+                select opt in "${options[@]}"
+                do
+                        case $opt in
+                                "Mainnet")
+                                        git clone https://github.com/ChihuahuaChain/chihuahua.git
+                                        #cd chihuahua
+                                        make install chihuahua/
+                                        chihuahuad init $MONIKER_NAME --chain-id chihuahua-1
+                                        sed -i 's/seeds = ""/seeds ="4936e377b4d4f17048f8961838a5035a4d21240c@chihuahua-seed-01.mercury-nodes.net:29540" ~/.chihuahua/config/config.toml
+                                        sed -i 's/persistent_peers = ""/persistent_peers = "b140eb36b20f3d201936c4757d5a1dcbf03a42f1@216.238.79.138:26656,19900e1d2b10be9c6672dae7abd1827c8e1aad1e@161.97.96.253:26656,c382a9a0d4c0606d785d2c7c2673a0825f7c53b2@88.99.94.120:26656,a5dfb048e4ed5c3b7d246aea317ab302426b37a1@137.184.250.180:26656,3bad0326026ca4e29c64c8d206c90a968f38edbe@128.199.165.78:26656,89b576c3eb72a4f0c66dc0899bec7c21552ea2a5@23.88.7.73:29538,38547b7b6868f93af1664d9ab0e718949b8853ec@54.184.20.240:30758,a9640eb569620d1f7be018a9e1919b0357a18b8c@38.146.3.160:26656,7e2239a0d4a0176fe4daf7a3fecd15ac663a8eb6@144.91.126.23:26656" ~/.chihuahua/config/config.toml
+                                        sed -i 's/minimum-gas-prices = ""/minimum-gas-prices = "0.01uhuahua"/g' ~/.chihuahua/config/app.toml
+                                        sudo rm -i ~/.chihuahua/config/genesis.json
+                                        wget https://github.com/ChihuahuaChain/mainnet/blob/main/genesis.json~/.chihuahua/config/
+                                        chihuahua start
+                                        cat > /etc/systemd/system/chihuahua.service
+                                        echo "[Unit]
+                                        Description=Chihuahua Node
+                                        After=network.target
+
+                                        [Service]
+                                        Type=simple
+                                        User=root
+                                        WorkingDirectory=/root/
+                                        ExecStart=/usr/bin/chihuahua start
+                                        Restart=on-failure
+                                        StartLimitInterval=0
+                                        RestartSec=3
+                                        LimitNOFILE=65535
+                                        LimitMEMLOCK=209715200
+
+                                        [Install]
+                                        WantedBy=multi-user.target" > /etc/systemd/system/chihuahua.service
+                                        #rm ~/.chihuahua/data/priv_validator_state.json
+                                        #wget http://135.181.60.250/akash/akashnet-2_$(date +"%Y-%m-%d").tar -P ~/.akash/data
+                                        #tar -xvf ~/.akash/data/akashnet-2_$(date +"%Y-%m-%d").tar
+                                        sudo systemctl enable chihuahua
+                                        sudo systemctl daemon-reload
+                                        sudo systemctl start chihuahua && journalctl -u chihuahua -f; break;;
+
+                                "Testnet")
+                                        break;;
+                                "Go Back")
+                                        break;;
+                        esac
+                done
 
             ;;
+
         "Comdex")
             echo "you chose choice $REPLY which is $opt"
             ;;
