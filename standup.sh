94f3452b47b877dd940129a880df700b44a37615
Joseph Chalabi <chalabi@Macintosh.local>
2022-01-13 21:26:33 -0700

    dded Comdex

--- standup.sh
+++ standup.sh
@@ -111,14 +111,19 @@ do
                         case $opt in
                                 "Mainnet")
                                         git clone https://github.com/ChihuahuaChain/chihuahua.git
-                                        #cd chihuahua
-                                        make install chihuahua/
-                                        chihuahuad init $MONIKER_NAME --chain-id chihuahua-1
+                                        cd chihuahua
+                                        git checkout v1.0.0
+                                        make install
+                                        cp ~/go/bin/chihuahuad /usr/bin/chihuahuad
+					echo "What would you like your node name to be?"
+					read NAME
+					echo "Your node $NAME is now set"
+                                        chihuahuad init $NAME --chain-id chihuahua-1
                                         sed -i 's/seeds = ""/seeds ="4936e377b4d4f17048f8961838a5035a4d21240c@chihuahua-seed-01.mercury-nodes.net:29540" ~/.chihuahua/config/config.toml
                                         sed -i 's/persistent_peers = ""/persistent_peers = "b140eb36b20f3d201936c4757d5a1dcbf03a42f1@216.238.79.138:26656,19900e1d2b10be9c6672dae7abd1827c8e1aad1e@161.97.96.253:26656,c382a9a0d4c0606d785d2c7c2673a0825f7c53b2@88.99.94.120:26656,a5dfb048e4ed5c3b7d246aea317ab302426b37a1@137.184.250.180:26656,3bad0326026ca4e29c64c8d206c90a968f38edbe@128.199.165.78:26656,89b576c3eb72a4f0c66dc0899bec7c21552ea2a5@23.88.7.73:29538,38547b7b6868f93af1664d9ab0e718949b8853ec@54.184.20.240:30758,a9640eb569620d1f7be018a9e1919b0357a18b8c@38.146.3.160:26656,7e2239a0d4a0176fe4daf7a3fecd15ac663a8eb6@144.91.126.23:26656" ~/.chihuahua/config/config.toml
                                         sed -i 's/minimum-gas-prices = ""/minimum-gas-prices = "0.01uhuahua"/g' ~/.chihuahua/config/app.toml
                                         sudo rm -i ~/.chihuahua/config/genesis.json
-                                        wget https://github.com/ChihuahuaChain/mainnet/blob/main/genesis.json~/.chihuahua/config/
+                                        wget https://github.com/ChihuahuaChain/mainnet/blob/main/genesis.json ~/.chihuahua/config/
                                         cat > /etc/systemd/system/chihuahua.service
                                         echo "[Unit]
                                         Description=Chihuahua Node
@@ -140,8 +145,8 @@ do
                                         #rm ~/.chihuahua/data/priv_validator_state.json
                                         #wget http://135.181.60.250/akash/akashnet-2_$(date +"%Y-%m-%d").tar -P ~/.akash/data
                                         #tar -xvf ~/.akash/data/akashnet-2_$(date +"%Y-%m-%d").tar
-                                        sudo systemctl enable chihuahua
                                         sudo systemctl daemon-reload
+                                        sudo systemctl enable chihuahua
                                         sudo systemctl start chihuahua && journalctl -u chihuahua -f; break;;
 
                                 "Testnet")
@@ -154,8 +159,45 @@ do
             ;;
 
         "Comdex")
-            echo "you chose choice $REPLY which is $opt"
-            ;;
+                                        git clone https://github.com/comdex-official/comdex
+                                        cd comdex
+                                        git checkout v0.0.4
+                                        make install
+                                        cp ~/go/bin/comdex /usr/bin/comdex
+					echo "What would you like your node name to be?"
+					read NAME
+					echo "Your node $NAME is now set"
+                                        comdex init $NAME
+                                        sed -i 's/seeds = ""/seeds ="aef35f45db2d9f5590baa088c27883ac3d5e0b33@3.108.102.92:26656" ~/.comdex/config/config.toml
+                                        sed -i 's/persistent_peers = ""/persistent_peers = "f74518ad134630da8d2405570f6a3639954c985f@65.0.173.217:26656,d478882a80674fa10a32da63cc20cae13e3a2a57@43.204.0.243:26656,61d743ea796ad1e1ff838c9e84adb38dfffd1d9d@15.235.9.222:26656,b8468f64788a17dbf34a891d9cd29d54b2b6485d@194.163.178.25:26656,d8b74791ee56f1b345d822f62bd9bc969668d8df@194.163.128.55:36656,81444353d70bab79742b8da447a9564583ed3d6a@164.68.105.248:26656,5b1ceb8110da4e90c38c794d574eb9418a7574d6@43.254.41.56:26656,98b4522a541a69007d87141184f146a8f04be5b9@40.112.90.170:26656,9a59b6dc59903d036dd476de26e8d2b9f1acf466@195.201.195.111:26656" ~/.comdex/config/config.toml
+                                        sed -i 's/minimum-gas-prices = ""/minimum-gas-prices = "0.01ucmdx"/g' ~/.comdex/config/app.toml
+                                        sudo rm -i ~/.comdex/config/genesis.json
+                                        wget https://github.com/comdex-official/networks/raw/main/mainnet/comdex-1/genesis.json ~/.comdex/config/
+                                        cat > /etc/systemd/system/comdex.service
+                                        echo "[Unit]
+                                        Description=Comdex Node
+                                        After=network.target
+
+                                        [Service]
+                                        Type=simple
+                                        User=$(whoami)
+                                        WorkingDirectory=/root/
+                                        ExecStart=/usr/bin/comdex start
+                                        Restart=on-failure
+                                        StartLimitInterval=0
+                                        RestartSec=3
+                                        LimitNOFILE=65535
+                                        LimitMEMLOCK=209715200
+
+                                        [Install]
+                                        WantedBy=multi-user.target" > /etc/systemd/system/comdex.service
+                                        #rm ~/.chihuahua/data/priv_validator_state.json
+                                        #wget http://135.181.60.250/akash/akashnet-2_$(date +"%Y-%m-%d").tar -P ~/.akash/data
+                                        #tar -xvf ~/.akash/data/akashnet-2_$(date +"%Y-%m-%d").tar
+                                        sudo systemctl daemon-reload
+                                        sudo systemctl enable comdex
+                                        sudo systemctl start comdex && journalctl -u comdex -f; break;;
+            
         "Dig")
             echo "you chose choice $REPLY which is $opt"
             ;;
