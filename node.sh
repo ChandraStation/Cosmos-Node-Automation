f1ea23f4016c0e1215aa7c11b5f64e70fb4d07ca
Bailey Bellows <parsec@macbook.lan>
2022-02-21 13:28:35 -0700

    changes

--- node.sh
+++ node.sh
@@ -125,9 +125,9 @@ function select_opt {
     echo $result
     return $result
 }
+
 case `select_opt "Akash" "Chihuahua" "Comdex" "Dig" "e-Money" "G-Bridge" "OmniFlix" "Osmosis" "Sentinel" "Logs" "Cancel"` in
     
-
 #AKASH    
     0)      tput setaf 1; echo 'What would you like your node name to be?'; tput sgr0
 	        read NAME
@@ -161,7 +161,7 @@ case `select_opt "Akash" "Chihuahua" "Comdex" "Dig" "e-Money" "G-Bridge" "OmniFl
             printf "\n"
 
 #Akash cont...
-            git clone https://github.com/ovrclk/akash -P /root/
+            git clone https://github.com/ovrclk/akash -P ~/root/
             cd akash 
             git checkout v0.14.1 
 	        make install 
@@ -214,8 +214,7 @@ EOF
 	        read WORKINGDIRECTORY
 	        tput setaf 3; echo 'Your Home Directory $WORKINGDIRECTORY is now set'; tput sgr0
 
-
-#Starting Progress Bar
+#Progress Bar
             printf '\nStarting...\n'
             function ProgressBar {
             let _progress=(${1}*100/${2}*100)/100
@@ -232,25 +231,25 @@ EOF
             do
             sleep 0.1
             ProgressBar ${number} ${_end}
-            done  &&
-            printf "\n"
+            done ||
+            printf "\n" ||
+
 
 #Chihuahua cont...
-            git clone https://github.com/ChihuahuaChain/chihuahua.git -P /root/
-            cd chihuahua
-            git checkout main
-            make install 
-            mv ~/go/bin/chihuahuad /usr/bin/ 
+            git clone https://github.com/ChihuahuaChain/chihuahua.git ||
+            cd chihuahua ||
+            git checkout v1.1.1 ||
+            make install ||
+            mv ~/go/bin/chihuahuad /usr/bin/ ||
             chihuahuad init $NAME --chain-id chihuahua-1 
-            cp mainnet/genesis.json ~/.chihuahua/config/genesis.json
             sed -i 's/seeds = ""/seeds = "4936e377b4d4f17048f8961838a5035a4d21240c@chihuahua-seed-01.mercury-nodes.net:29540"/g' ~/.chihuahua/config/config.toml 
             sed -i 's/persistent_peers = ""/persistent_peers = "b140eb36b20f3d201936c4757d5a1dcbf03a42f1@216.238.79.138:26656,19900e1d2b10be9c6672dae7abd1827c8e1aad1e@161.97.96.253:26656,c382a9a0d4c0606d785d2c7c2673a0825f7c53b2@88.99.94.120:26656,a5dfb048e4ed5c3b7d246aea317ab302426b37a1@137.184.250.180:26656,3bad0326026ca4e29c64c8d206c90a968f38edbe@128.199.165.78:26656,89b576c3eb72a4f0c66dc0899bec7c21552ea2a5@23.88.7.73:29538,38547b7b6868f93af1664d9ab0e718949b8853ec@54.184.20.240:30758,a9640eb569620d1f7be018a9e1919b0357a18b8c@38.146.3.160:26656,7e2239a0d4a0176fe4daf7a3fecd15ac663a8eb6@144.91.126.23:26656"/g' ~/.chihuahua/config/config.toml
-            sed -i 's/laddr = "tcp://127.0.0.1:26657"/laddr = "tcp://127.0.0.1:86657"/g' ~/.chihuahua/config/config.toml 
-            sed -i 's/laddr = "tcp://0.0.0.0:26656"/laddr = "tcp://0.0.0.0:86656"/g' ~/.chihuahua/config/config.toml 
+            sed -i 's#laddr = "tcp://127.0.0.1:26657"#laddr = "tcp://127.0.0.1:86657"#g' ~/.chihuahua/config/config.toml
+            sed -i 's#laddr = "tcp://0.0.0.0:26656"#laddr = "tcp://0.0.0.0:86656"#g' ~/.chihuahua/config/config.toml 
             sed -i 's/address = "0.0.0.0:9090"/address = "0.0.0.0:8090"/g' ~/.chihuahua/config/app.toml 
             sed -i 's/address = "0.0.0.0:9091"/address = "0.0.0.0:8091"/g' ~/.chihuahua/config/app.toml
             sed -i 's/minimum-gas-prices = "0stake"/minimum-gas-prices = "0.025uhuahua"/g' ~/.chihuahua/config/app.toml 
-            #wget -O ~/.chihuahua/config/genesis.json https://raw.githubusercontent.com/ChihuahuaChain/mainnet/main/genesis.json -P ~/.chihuahua/config/ 
+            wget -O ~/.chihuahua/config/genesis.json https://raw.githubusercontent.com/ChihuahuaChain/mainnet/main/genesis.json -P ~/.chihuahua/config/ 
             cat << EOF > /etc/systemd/system/chihuahuad.service 
 [Unit] 
 Description=Chihuahua Node
@@ -270,6 +269,10 @@ LimitMEMLOCK=209715200
 [Install]
 WantedBy=multi-user.target
 EOF
+            rm -rf .chihuahua/data/
+            wget https://tendermint-snapshots.polkachu.xyz/chihuahua/chihuahua_887041.tar.lz4 -P ~/.chihuahua/
+            lz4 ~/.chihuahua/data/chihuahua_887041.tar.lz4 
+            tar -xvf ~/.chihuahua/data/chihuahua_887041.tar
             systemctl daemon-reload 
             systemctl enable chihuahuad
             systemctl start chihuahuad
