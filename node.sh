ab544de99985ecdf0662b4db0092d0a134ce81ab
Bailey Bellows <parsec@macbook.lan>
2022-02-21 16:30:29 -0700

    changes

--- node.sh
+++ node.sh
@@ -317,8 +317,8 @@ EOF
             git checkout v0.0.4
             make install 
             cp ~/go/bin/comdex /usr/bin/comdex 
-            comdex init $NAME 
-            sed -i 's/seeds = ""/seeds ="aef35f45db2d9f5590baa088c27883ac3d5e0b33@3.108.102.92:26656"/g' ~/.comdex/config/config.toml 
+            comdex init $NAME --home $WORKINGDIRECTORY/.comdex/
+            sed -i 's/seeds = ""/seeds ="aef35f45db2d9f5590baa088c27883ac3d5e0b33@3.108.102.92:26656"/g' $WORKINGDIRECTORY/.comdex/config/config.toml 
             sed -i 's/persistent_peers = ""/persistent_peers = "f74518ad134630da8d2405570f6a3639954c985f@65.0.173.217:26656,d478882a80674fa10a32da63cc20cae13e3a2a57@43.204.0.243:26656,61d743ea796ad1e1ff838c9e84adb38dfffd1d9d@15.235.9.222:26656,b8468f64788a17dbf34a891d9cd29d54b2b6485d@194.163.178.25:26656,d8b74791ee56f1b345d822f62bd9bc969668d8df@194.163.128.55:36656,81444353d70bab79742b8da447a9564583ed3d6a@164.68.105.248:26656,5b1ceb8110da4e90c38c794d574eb9418a7574d6@43.254.41.56:26656,98b4522a541a69007d87141184f146a8f04be5b9@40.112.90.170:26656,9a59b6dc59903d036dd476de26e8d2b9f1acf466@195.201.195.111:26656"/g' ~/.comdex/config/config.toml
             sed -i 's/minimum-gas-prices = ""/minimum-gas-prices = "0.01ucmdx"/g' ~/.comdex/config/app.toml
             sed -i 's#laddr = "tcp://127.0.0.1:26657"#laddr = "tcp://127.0.0.1:56657"#g' ~/.comdex/config/config.toml 
@@ -347,7 +347,7 @@ LimitMEMLOCK=209715200
 WantedBy=multi-user.target
 EOF
             rm -rf .comdex/data/
-            wget https://cosmos-snap.staketab.com/comdex -P ~/.chihuahua/data
+            wget https://cosmos-snap.staketab.com/comdex -P ~/.comdex/data
             tar -xvf ~/.comdex/data/comdex
             sudo systemctl daemon-reload
             sudo systemctl enable comdex
